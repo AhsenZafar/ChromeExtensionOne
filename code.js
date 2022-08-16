16398e829ffafb3a60279b43d9eb4b47db72027b
AhsenZafar <100182384+AhsenZafar@users.noreply.github.com>
2022-08-15 18:08:37 -0600

    new function
    create function to render leads and save them into html list

--- code.js
+++ code.js
@@ -2,13 +2,34 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 let myLeads =[]
 const ulEl = document.getElementById("ul-el")
+
+// save input leads into myleads array
 inputBtn.addEventListener("click", function(){
     
     myLeads.push(inputEl.value)
+    inputEl.value =""
+    renderLeads()
 })
 
+// store leads into listItems variable
+function renderLeads(){
+    let listItems =""
+    for (let i =0; i < myLeads.length; i++){
+        listItems += `
+            <li>
+                <a target = 'blank' href = '${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            <li>
+            `
+        
+        
+        
+    }
+// display listItems onto page by making them the content for the unordered list
+    ulEl.innerHTML = listItems
 
-for (let i =0; i < myLeads.length; i++){
-    ulEl.textContent += myLeads[i] + ""
+
+
 }
 
